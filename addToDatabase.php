138e05d5467038535710cf3891174bad26fe6286
Your Name <you@example.com>
2025-10-13 16:22:04 -0400

    Added file that adds data to database

---
+++ addToDatabase.php
@@ -0,0 +1,25 @@
+ <?php
+$servername = "localhost";
+$username = "testUser";
+$password = "12345";
+$dbname = "verification";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO users (username, userid, password)
+VALUES ('Brandon', 1, 'bmc52')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
