856582e5dd00d8506f2920a8c4d3a1575080f2b4
Matthew Hoffman <mhoffman5831@gmail.com>
2025-10-14 13:43:36 -0400

    updates

--- test.php
+++ test.php
@@ -1,10 +1,18 @@
 #!/usr/bin/php
 <?php
+/*
 echo "test file running\n";
 echo $argv[1];
 //$output;
 //exec("php test2.php $argv[1]",$output);
-include("test2.php");
+//include("test2.php");
 runTest($argv[1])
-//echo($output[0]);
+	//echo($output[0]);
+	//
+	//
+	//
+ */
+include("webServerClient.php");
+sendRabbitWeb("login","user","pass");
+
 ?>
