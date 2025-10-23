b978f27c3c867d1159bc555c14ac85ea0799bfb0
Your Name <you@example.com>
2025-10-23 12:29:09 -0400

    Added file to fetch specific data

---
+++ fetchData.php
@@ -0,0 +1,12 @@
+<?php
+
+$servername="172.27.80.151";
+$username="BRANDON";
+$password="bmc52";
+$dbname = "verification";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$query = "SELECT username FROM users WHERE username = 'brandon' AND password = 'bmc52'";
+
+return $query;
